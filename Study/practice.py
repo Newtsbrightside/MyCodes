afb99a50f7869a99ca5ad7694fab6c120a26be11
Yunus Zulfikar <nuszulfikar@example.com>
2025-10-05 15:42:19 +0700

    practicing for exam

---
+++ Study/practice.py
@@ -0,0 +1,65 @@
+# x = 'zhejiang'
+# y = 200 % 3 + 9//7
+# print(x[3:6] * y)
+# word = "jia"
+# print(word[0:3] * 3)
+# num = 1 % 4
+# print(num)
+# ten = "10"
+# five = 5
+# #print(five + ten) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# print(str(five) + ten)
+# print(int(ten) + five)
+# print(float(ten) + five)
+# print(ten * 5) # 1010101010
+# print(ten + '5') # 105
+# print(ten * int(ten)) # 10101010100
+# print(ten + str(five)) # 105
+# #print(ten * float(ten)) # TypeError: can't multiply sequence by non-int of type 'float'
+# print(ten + str(float(ten))) # 1010.0
+# print(ten * int(float(ten))) # 10101010101010101010
+# print(ten + str(int(float(ten)))) # 1010   
+# print(10 // 9)
+
+'''pi = 3.14159
+radius = float(input("Enter the radius of the circle: ").strip())
+area = pi * radius ** 2
+print("The area of the circle is:", area)'''
+# C = float(input("Enter temperature in Celsius: ").strip())
+# 1F = (C * 9/5) + 32
+# print("Temperature in Fahrenheit:", F)
+'''txt = "The quick brown fox jumps over the lazy dog"
+print(txt[-1::-1])
+print(txt[::2])
+x = 5
+y = 10
+z = [x, y]
+print(max(z))  # Output: 10
+print(min(z))  # Output: 5
+print(len(z))  # Output: 2
+print(sum(z))  # Output: 15
+print(sorted(z))  # Output: [5, 10]
+print(sorted(z, reverse=True))  # Output: [10, 5]
+z.append(15)
+a = z.copy()
+print(a)  # Output: [5, 10, 15]
+z.remove(10)
+print(z)  # Output: [5, 15]
+print(z.count(5))  # Output: 1
+z.extend([20, 25])
+a = set(a)
+z = set(z)
+print(z)  # Output: {5, 15, 20, 25}
+print(a)  # Output: {5, 10, 15}
+a.update(z)
+print(a)  # Output: {5, 10, 15, 20, 25}
+a = list(a)
+print(a.pop(2)) # Output: 15
+print(a)  # Output: [5, 10, 20]
+a = dict(enumerate(a, start=1))
+print(a)  # Output: {1: 'five', 2: 'ten', 3: 'twenty'}
+print(a.keys())  # Output: dict_keys([1, 2, 3])
+print(a.values())  # Output: dict_values(['five', 'ten', 'twenty'])
+print(a.items())  # Output: dict_items([(1, 'five'), (2, 'ten'), (3, 'twenty')])
+print(a.get(2))  # Output: ten
+print(a.get(5))  # Output: None'''
